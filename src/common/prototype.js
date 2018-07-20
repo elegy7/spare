6189fcec56d5ad4773e264014131ad2a53e6b09e
xiaohx <xiaohx@haiyungroup.com>
2018-07-20 17:25:54 +0800

    去除分号

--- src/common/prototype.js
+++ src/common/prototype.js
@@ -1,4 +1,4 @@
-;(function() {
+; (function () {
     /*eslint-disable*/
     //扩展日期格式化方法
     Date.prototype.format = function (fmt) { //author: meizz
@@ -10,11 +10,11 @@
             "s+": this.getSeconds(), //秒
             "q+": Math.floor((this.getMonth() + 3) / 3), //季度
             "S": this.getMilliseconds() //毫秒
-        };
-        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        }
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length))
         for (var k in o)
-            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-        return fmt;
+            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)))
+        return fmt
     }
     //扩展字符串去空格方法
     String.prototype.trim = function () {
